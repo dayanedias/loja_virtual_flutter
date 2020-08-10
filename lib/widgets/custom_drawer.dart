45f6afd97e393f0ff29d3382e48099866d3f257f
Dayane Rodrigues <dayane.rodrigues@softplan.com.br>
2020-08-10 20:41:33 -0300

    :sparkles: Create a menu in drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_cliente/tiles/drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
@@ -34,10 +35,36 @@ class CustomDrawer extends StatelessWidget {
                       top: 8.0,
                       left: 0.0,
                       child: Text("Flutter's \nClothing", style: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold),),
-                    )
+                    ),
+                    Positioned(
+                      left: 0.0,
+                      bottom: 0.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Olá, ", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),),
+                          GestureDetector(
+                            child: Text("Entre ou cadastre-se >",
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onTap: () {
+
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
+              Divider(),
+              DrawerTile(Icons.home, "Início"),
+              DrawerTile(Icons.list, "Produtos"),
+              DrawerTile(Icons.location_on, "Lojas"),
+              DrawerTile(Icons.playlist_add_check, "Meus pedidos"),
             ],
           ),
         ],
